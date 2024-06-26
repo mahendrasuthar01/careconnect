b4f32286a24da4366b3bdc4cbcab831f1294ecf4
Prasnsha Mishra <prasnsham.brainerhub@gmail.com>
2024-06-26 18:36:15 +0530

    Removed getting all validated_data

--- careconnect/healthcare/views.py
+++ careconnect/healthcare/views.py
@@ -100,30 +100,14 @@ class HospitalViewSet(viewsets.ModelViewSet):
                 destination.write(chunk)
         return 'media/uploaded_files/' + file.name
 
+
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             files = request.FILES.get('files', None)
-            if files:
-                image_url = self.save_file(files)
-            else:
-                image_url = None
+            image_url = self.save_file(files) if files else None
 
-            hospital = Hospital(
-                category_id=serializer.validated_data.get('category_id'),
-                name=serializer.validated_data.get('name'),
-                review_id=serializer.validated_data.get('review'),
-                website=serializer.validated_data.get('website'),
-                phone_number=serializer.validated_data.get('phone_number'),
-                email=serializer.validated_data.get('email'),
-                location_id=serializer.validated_data.get('location_id'),
-                working_time_id=serializer.validated_data.get('working_time_id'),
-                address=serializer.validated_data.get('address'),
-                specialist=serializer.validated_data.get('specialist'),
-                is_favorite=serializer.validated_data.get('is_favorite'),
-                files=image_url
-            )
-            hospital.save()
+            hospital = serializer.save(files=image_url)
 
             response_data = serializer.data
             response_data['files'] = None
@@ -151,6 +135,7 @@ class DoctorViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Doctor.objects.all()
+    
 
     def save_file(self, file):
         file_path = os.path.join(settings.MEDIA_ROOT, 'doctors_files', file.name)
@@ -160,33 +145,14 @@ class DoctorViewSet(viewsets.ModelViewSet):
                 destination.write(chunk)
         return 'media/doctors_files/' + file.name
     
+    
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             files = request.FILES.get('files', None)
-            if files:
-                file_url = self.save_file(files)
-            else:
-                file_url = None
+            file_url = self.save_file(files) if files else None
 
-            doctor = Doctor(
-                user_id=serializer.validated_data.get('user_id'),
-                name=serializer.validated_data.get('name'),
-                speciality_id=serializer.validated_data.get('speciality_id'),
-                working_time_id=serializer.validated_data.get('working_time_id'),
-                about=serializer.validated_data.get('about'),
-                location_id=serializer.validated_data.get('location_id'),
-                is_favorite=serializer.validated_data.get('is_favorite'),
-                is_active=True,
-                total_experience=serializer.validated_data.get('total_experience'),
-                total_patients=serializer.validated_data.get('total_patients'),
-                review_id=serializer.validated_data.get('review_id'),
-                hospital_id=serializer.validated_data.get('hospital_id'),
-                files=file_url
-            )
-            doctor.sign_up_date = timezone.now()
-
-            doctor.save()
+            doctor = serializer.save(files=file_url, is_active=True)
 
             response_data = serializer.data
             response_data['files'] = None
@@ -197,8 +163,8 @@ class DoctorViewSet(viewsets.ModelViewSet):
             return Response(response_data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
     
-
     def destroy(self, request, *args, **kwargs):
         try:
             doctor = self.get_object()
