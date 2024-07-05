69bee07614f6e22a8610fd8244d728d3d7344a20
Prasnsha Mishra <prasnsham.brainerhub@gmail.com>
2024-07-05 18:32:45 +0530

    Fixed conflicts

--- careconnect/appointments/models.py
+++ careconnect/appointments/models.py
@@ -1,6 +1,6 @@
 from mongoengine import Document, StringField, IntField, ReferenceField, CASCADE, DateTimeField, BooleanField
 from healthcare.models import Doctor
-from accounts.models import Patient
+from accounts.models import Patient, User
 from mongoengine.signals import pre_save
 import random, string
 from datetime import datetime
@@ -46,8 +46,10 @@ class Appointment(Document):
     created_at = DateTimeField(required=True, auto_now_add=True)
     confirm = BooleanField(default=False)
     status = IntField(max_length=100, choices=AppointmentStatusChoice.CHOICES)
+    status = IntField(max_length=100, choices=AppointmentStatusChoice.CHOICES)
     cancellation_reason = StringField(max_length=500)
     cancellation_time = DateTimeField(required=False, auto_now_add=True)
+    user_id = ReferenceField(User, reverse_delete_rule=CASCADE)
 
     @property
     def doctor(self):
